25101caa978c8cc7f1a1de0353460c89ae792419
Omar Zapata <andresete.chaos@gmail.com>
2024-03-26 16:48:29 -0500

    implemented load metadata to elastic search

---
+++ load_es.py
@@ -0,0 +1,47 @@
+from mohan.Similarity import Similarity
+from pymongo import MongoClient
+
+
+es_index = "openalex_index"
+
+#creating the instance
+s = Similarity(es_index,es_uri= "http://localhost:9200",
+                 es_auth = ('elastic', 'colav'))
+
+#taking openalex as example.
+openalex = MongoClient()["openalex_new"]["works"].find({},{"title":1,"primary_location.source":1,"publication_year":1,"biblio":1,"authorships":1,"_id":1})
+
+#example inserting documents to the Elastic Search index.
+bulk_size = 100
+
+s.delete_index(es_index)
+
+es_entries = []
+counter = 0
+for i in openalex:
+    work = {}
+    work["title"] = i["title"]
+    if "primary_location" in i.keys() and i["primary_location"]:
+        if i["primary_location"]["source"]:
+            work["source"] = i["primary_location"]["source"]["display_name"]
+        work["source"] = ""
+    else:
+        work["source"] = ""
+    work["year"] = i["publication_year"]
+    work["volume"] = i["biblio"]["volume"]
+    work["issue"] = i["biblio"]["issue"]
+    work["first_page"] = i["biblio"]["first_page"]
+    work["last_page"] = i["biblio"]["last_page"]
+    authors = []
+    for author in i['authorships']:
+        if "display_name" in author["author"].keys():
+            authors.append(author["author"]["display_name"])
+    work["authors"] = authors
+    
+    entry = {"_index": es_index,
+                "_id": str(i["_id"]),
+                "_source": work}
+    es_entries.append(entry)
+    if len(es_entries) == bulk_size:
+        s.insert_bulk(es_entries)
+        es_entries = []
